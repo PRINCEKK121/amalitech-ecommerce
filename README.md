ff4828690a4f3dfc06dd2dda3e8d7dfdd50883ff
kyonkopa <kyonkopa@gmail.com>
2020-11-21 10:50:34 +0000

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# amalitech-ecommerce-application
+
+Empty project.
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+```
+
+## Running
+
+```sh
+node dist/bundle.js
+```
+
+## Links
+
+[Site to reproduce](https://www.vineyardvines.com/boys-fashion-knits/boys-stillwater-sherpa-quarter-zip/3K001154.html?dwvar_3K001154_color=252&cgid=new-arrivals-boys#start=2&cgid=new-arrivals-boys)
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
